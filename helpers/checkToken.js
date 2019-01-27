cf7066d86442574cc1b44b980ad8b55f84f61f8c
saroj <sarojmhrzn@gmail.com>
2019-01-27 13:04:57 +0545

    hotfix

--- helpers/checkToken.js
+++ helpers/checkToken.js
@@ -2,7 +2,7 @@
 
 function checkTokenInHeader(req, res, next) {
     const headerToken = req.headers.token;
-    if (headerToken === '1517099047' || req.url === 'v1/hello') {
+    if (headerToken === '1517099047' || req.url === '/') {
         next();
     } else {
         throw new Error("Token mismatched!");
